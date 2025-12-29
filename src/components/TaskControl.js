d1423d5ca87ed71bea1b20151ee504d123673278
Mateusz Chrabolowski <mateusz.chrabolowski@gmail.com>
2025-12-29 01:26:59 +0100

    adding some visual changes and downgrade a react version to 18

--- src/components/TaskControl.js
+++ src/components/TaskControl.js
@@ -2,13 +2,23 @@ function TaskControls({ showCompleted, setShowCompleted, finishAll, tasks }) {
   const allDone = tasks.length > 0 && tasks.every(t => t.done);
 
   return (
-        <div className="card__header">
-            <div className="controls">
-                <button>Ukryj ukończone</button>
-                <button>Ukończ wszystkie</button>
-            </div>
-        </div>
-        );
-    }
+    <div className="card__header">
+      <div className="controls">
+        <button
+          onClick={() => setShowCompleted(prev => !prev)}
+        >
+          {showCompleted ? "Ukryj ukończone" : "Pokaż ukończone"}
+        </button>
+
+        <button
+          onClick={finishAll}
+          disabled={allDone}
+        >
+          Ukończ wszystkie
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default TaskControls;
